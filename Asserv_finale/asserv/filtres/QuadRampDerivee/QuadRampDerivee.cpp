da29f017465db8edc4cec04c6b22cc99c4aee91a
Francois <francois.prugniel@loria.fr>
2011-09-24 17:38:30 +0200

    reecriture de la quadrampderivee

--- Asserv_finale/asserv/filtres/QuadRampDerivee/QuadRampDerivee.cpp
+++ Asserv_finale/asserv/filtres/QuadRampDerivee/QuadRampDerivee.cpp
@@ -1,87 +1,90 @@
 #include "QuadRampDerivee.h"
-#include "../Utils/Utils.h"
 
 //--------------------------------------------------------
 //                      QUADRAMPm
 //-------------------------------------------------------
-int64_t QuadRampDerivee::filtre(int64_t consigne, int64_t position_actuelle , int64_t vitesse)
-{
-    /*
-     *  Diviseur permettant de ne pas changer de consigne a chaque iteration
-     *    Et donc de laisser du temps au PID pour arriver a la position voulue
-     */
-    if( diviseurFrequence > 1 )
-    {
-      if( --divisdeurFrequenceCompteur > 0 ) 
-      {
-        return prevConsignePosition;
-      } 
-      divisdeurFrequenceCompteur = diviseurFrequence;
-    }
-    
-    // Reset de certains "flag" qui seront determines au cours de la fonction
-   arrivee = false;
-
-
-   //Calcul de la position du pivot
-   
-    char sens = ( consigne - position_actuelle >= 0 ) ? 1 : -1;
-    if(sens == 1) {
-        position_pivot = consigne + ( ( vitesse >= 0 ) ? -1 : 1) * ( ((vitesse * vitesse )/(2*derivee_2nd_neg_av)) + abs(vitesse) * gainAnticipation_av );
-    } else {
-        position_pivot = consigne + ( ( vitesse >= 0 ) ? -1 : 1) * ( ((vitesse * vitesse )/(2*derivee_2nd_neg_ar)) + abs(vitesse) * gainAnticipation_ar );
-    }
-    
-    
-
-   //Calcul de la consigne d'acceleration  
-   int64_t acceleration_consign;
-   
-   if( position_pivot >= position_actuelle )
-     acceleration_consign = (sens == 1) ? derivee_2nd_pos_av : derivee_2nd_neg_ar;
-   else
-     acceleration_consign = (sens == 1) ? -derivee_2nd_neg_av : -derivee_2nd_pos_ar;
+QuadRampDerivee::QuadRampDerivee(bool isDistance) {
   
+  if (isDistance) {
+    // Derivees premieres de la consigne ( dans notre cas, la vitesse )
+    derivee_1ier_pos = DIST_QUAD_1ST_POS;
+    derivee_1ier_neg = DIST_QUAD_1ST_NEG;
 
-/* Pkoi dans le calcul de la position du pivot tu utilise la vitesse ??? &#65533;a serait pas plus logiquer d'utiliser le sens qu'on vient de calculer? 
-    D'ailleurs &#65533;a peut se simplifier vachement ou bien?? * /
-   
-   
-   //EXEMPLE : 
-    char sens = ( consigne - position_actuelle >= 0 ) ? -1 : 1;
-    int64_t d2_neg = ( sens == -1 ) ? derivee_2nd_neg_av : derivee_2nd_neg_ar;
-    double gainAnticipation = ( sens == -1 ) ? gainAnticipation_av : gainAnticipation_ar;
-    position_pivot = consigne + sens * ( ((vitesse * vitesse )/(2*d2_neg)) + abs(vitesse) * gainAnticipation );
-    
-   int64_t acceleration_consign;
-   
-   if( position_pivot >= position_actuelle )
-     acceleration_consign = (sens == -1) ? derivee_2nd_pos_av : derivee_2nd_neg_ar;
-   else
-     acceleration_consign = (sens == -1) ? -derivee_2nd_neg_av : -derivee_2nd_pos_ar;
-    
-*/
+    // Derivees secondes de la consigne ( dans notre cas, l'acceleration ) 
+    derivee_2nd_pos_av = DIST_QUAD_AV_2ND_ACC;
+    derivee_2nd_neg_av = DIST_QUAD_AV_2ND_DEC;
+     
+    //la meme chose pour le deplacement vers l'arriere
+    derivee_2nd_pos_ar = DIST_QUAD_AR_2ND_ACC;
+    derivee_2nd_neg_ar = DIST_QUAD_AR_2ND_DEC;
+     
+    //On ajoute de l' "anticipation " pour eviter que la vitesse ne chute brutalement a zero quand le point d'arrivee arrive....( bien pourri ca dis donc -_-" )
+    gainAnticipation_av = DIST_QUAD_AV_ANTICIPATION_GAIN_COEF;
+    gainAnticipation_ar = DIST_QUAD_AR_ANTICIPATION_GAIN_COEF;
+     
+    //Pour eviter les vibrations a l'arrivee
+    tailleFenetreArrivee = DIST_TAILLE_FENETRE_ARRIVEE;
+  } else {
+    // Derivees premieres de la consigne ( dans notre cas, la vitesse )
+    derivee_1ier_pos = ANGLE_QUAD_1ST_POS;
+    derivee_1ier_neg = ANGLE_QUAD_1ST_POS;
 
+    // Derivees secondes de la consigne ( dans notre cas, l'acceleration ) 
+    derivee_2nd_pos_av = ANGLE_QUAD_2ND_ACC;
+    derivee_2nd_neg_av = ANGLE_QUAD_2ND_DEC;
+     
+    //la meme chose pour le deplacement vers l'arriere
+    derivee_2nd_pos_ar = ANGLE_QUAD_2ND_ACC;
+    derivee_2nd_neg_ar = ANGLE_QUAD_2ND_DEC;
+     
+    //On ajoute de l' "anticipation " pour eviter que la vitesse ne chute brutalement a zero quand le point d'arrivee arrive....( bien pourri ca dis donc -_-" )
+    gainAnticipation_av = ANGLE_QUAD_ANTICIPATION_GAIN_COEF;
+    gainAnticipation_ar = ANGLE_QUAD_ANTICIPATION_GAIN_COEF;
+     
+    //Pour eviter les vibrations a l'arrivee
+    tailleFenetreArrivee = ANGLE_TAILLE_FENETRE_ARRIVEE;
+  }
   
-  
-   //Calcul de la consigne de vitesse  
-   int64_t consigneVitesse = prevConsigneVitesse + acceleration_consign;
-   consigneVitesse = Utils::constrain(consigneVitesse,-derivee_1ier_neg, derivee_1ier_pos);
-      prevConsigneVitesse = consigneVitesse;
+  prevConsigneVitesse = 0;
+  arrivee=true;
+}
+
+QuadRampDerivee::~QuadRampDerivee() {};
+
+int64_t QuadRampDerivee::filtre(int64_t consigne, int64_t position_actuelle , int64_t vitesse) {
+    
+  // Reset du flag "arrivee" signalant que la consigne est atteinte
+  arrivee = false;
+
+  //Calcul de la position du pivot
+  char sens = ( consigne - position_actuelle >= 0 ) ? 1 : -1;
+  int64_t position_pivot;
+  if(sens == 1) {
+    position_pivot = consigne + ( ( vitesse >= 0 ) ? -1 : 1) * ( ((vitesse * vitesse )/(2*derivee_2nd_neg_av)) + abs(vitesse) * gainAnticipation_av );
+  } else {
+    position_pivot = consigne + ( ( vitesse >= 0 ) ? -1 : 1) * ( ((vitesse * vitesse )/(2*derivee_2nd_neg_ar)) + abs(vitesse) * gainAnticipation_ar );
+  }
+    
+  //Calcul de la consigne d'acceleration  
+  int64_t acceleration_consign;
+   
+  if( position_pivot >= position_actuelle )
+    acceleration_consign = (sens == 1) ? derivee_2nd_pos_av : derivee_2nd_neg_ar;
+  else
+    acceleration_consign = (sens == 1) ? -derivee_2nd_neg_av : -derivee_2nd_pos_ar;
+
+  //Calcul de la consigne de vitesse  
+  int64_t consigneVitesse = prevConsigneVitesse + acceleration_consign;
+  consigneVitesse = Utils::constrain(consigneVitesse,-derivee_1ier_neg, derivee_1ier_pos);
+  prevConsigneVitesse = consigneVitesse;
       
-      
-    
-   // On verifie si on est dans la fenetre d'arrivee
-   if ( abs(consigne-position_actuelle) < tailleFenetreArrivee )
-   {
-      prevConsigneVitesse = 0; // On reset la consigne precedente 
-     arrivee = true;
-     return consigne;
-   }
+  // On verifie si on est dans la fenetre d'arrivee
+  if ( abs(consigne-position_actuelle) < tailleFenetreArrivee ) {
+    prevConsigneVitesse = 0; // On reset la consigne precedente 
+    arrivee = true;
+    return consigne;
+  }
    
-  
-   //Calcul de la consigne de position
-   prevConsignePosition = position_actuelle+consigneVitesse;
- 
-   return prevConsignePosition; 
+  //On retourne la consigne de position
+  return position_actuelle+consigneVitesse; 
 }
--- Asserv_finale/asserv/filtres/QuadRampDerivee/QuadRampDerivee.h
+++ Asserv_finale/asserv/filtres/QuadRampDerivee/QuadRampDerivee.h
@@ -2,109 +2,45 @@
 #define QUADRAMPDERIVEE
 
 #include "../Filtre.h"
+#include "../../Utils/Utils.h"
 
 class QuadRampDerivee : public Filtre
  {
-   public:
-   
-     QuadRampDerivee()
-     {
-       diviseurFrequence = 0;
-       divisdeurFrequenceCompteur = diviseurFrequence;
-       prevPosition = 0;
-       prevConsigneVitesse = 0;
-       prevConsignePosition = 0;
-       tailleFenetreArrivee = 0;
-       arrivee=true;
-       gainAnticipation_av = 0;
-       gainAnticipation_ar = 0;
-     }
-     
-     
-     // configuration
-     void setDerivee1ier( int64_t positive, int64_t negative )
-     {
-         derivee_1ier_pos = positive;
-         derivee_1ier_neg = negative;
-     }
-     
-     int64_t getDerivee1ierPos() { return derivee_1ier_pos; }
-     int64_t getDerivee1ierNeg() { return derivee_1ier_neg; }
-     
-     
+  public:
+    
+    QuadRampDerivee(bool isDistance);
+    ~QuadRampDerivee();
+    bool isRampFinished() { return arrivee; }
 
-     void setDerivee2ndAv( int64_t positive, int64_t negative )
-     {
-         derivee_2nd_pos_av = positive;
-         derivee_2nd_neg_av = negative;
-     }
-     
-     void setDerivee2ndAr( int64_t positive, int64_t negative )
-     {
-         derivee_2nd_pos_ar = positive;
-         derivee_2nd_neg_ar = negative;
-     }
-     
-     int64_t getDerivee2ndPosAv() { return derivee_2nd_pos_av; }
-     int64_t getDerivee2ndNegAv() { return derivee_2nd_neg_av; }
-     int64_t getDerivee2ndPosAr() { return derivee_2nd_pos_ar; }
-     int64_t getDerivee2ndNegAr() { return derivee_2nd_neg_ar; }
-     
-     
-     void setCoeffGainAnticipationAv( double value) { gainAnticipation_av = value; }
-     double getCoeffGainAnticipationAv() { return gainAnticipation_av; }
-     void setCoeffGainAnticipationAr( double value) { gainAnticipation_ar = value; }
-     double getCoeffGainAnticipationAr() { return gainAnticipation_ar; }
-     
-     void setTailleFenetreArrivee(int64_t taille) { tailleFenetreArrivee = taille; }
-     int getTailleFenetreArrivee() { return tailleFenetreArrivee; }
-     
-     
-     bool isRampFinished() { return arrivee; }
-
-     int64_t filtre(int64_t consigne, int64_t position_actuelle , int64_t vitesse);
-     
-     int64_t getPositionPivot(){ return position_pivot;}
-     
-     
+    int64_t filtre(int64_t consigne, int64_t position_actuelle , int64_t vitesse);     
  
-   private:
-   
-     // Diviseur ( pour ne pas appliquer une consigne differente a chaque iteration )
-     int diviseurFrequence;
-     int divisdeurFrequenceCompteur;
-     int64_t position_pivot;
+  private:
      
-     // Pour calcul de la vitesse ( derivee )
-     int64_t prevPosition;
-     
-     // Derivees premieres de la consigne ( dans notre cas, la vitesse )
-     int64_t derivee_1ier_pos; //En avant ( positif )
-     int64_t derivee_1ier_neg; // En arriere (negatif )
+    // Derivees premieres de la consigne ( dans notre cas, la vitesse )
+    int64_t derivee_1ier_pos; //En avant ( positif ) // Vient de config.h
+    int64_t derivee_1ier_neg; // En arriere (negatif )  // Vient de config.h
 
     // Derivees secondes de la consigne ( dans notre cas, l'acceleration ) 
-     int64_t derivee_2nd_pos_av; //En avant ( positif )
-     int64_t derivee_2nd_neg_av; // En arriere (negatif - deceleration)
+    int64_t derivee_2nd_pos_av; //En avant ( positif )  // Vient de config.h
+    int64_t derivee_2nd_neg_av; // En arriere (negatif - deceleration) // Vient de config.h
      
-     //la meme chose pour le deplacement vers l'arriere
-     int64_t derivee_2nd_pos_ar;
-     int64_t derivee_2nd_neg_ar;
+    //la meme chose pour le deplacement vers l'arriere
+    int64_t derivee_2nd_pos_ar; // Vient de config.h
+    int64_t derivee_2nd_neg_ar; // Vient de config.h
      
-     //On ajoute de l' "anticipation " pour eviter que la vitesse ne chute brutalement a zero quand le point d'arrivee arrive....( bien pourri ca dis donc -_-" )
-     double gainAnticipation_av;
-     double gainAnticipation_ar;
+    //On ajoute de l' "anticipation " pour eviter que la vitesse ne chute brutalement a zero quand le point d'arrivee arrive....( bien pourri ca dis donc -_-" )
+    double gainAnticipation_av; // Vient de config.h
+    double gainAnticipation_ar; // Vient de config.h
 
-     // Sauvegarde des valeurs precedentes
-     int64_t prevConsigneVitesse;
-     int64_t prevConsignePosition;
+    // Sauvegarde de la vitesse precedente
+    int64_t prevConsigneVitesse; // Stocke la vitesse de l'itération précédente
      
-     //Pour eviter les vibrations a l'arrivee
-     int64_t tailleFenetreArrivee;
+    //Pour eviter les vibrations a l'arrivee
+    int64_t tailleFenetreArrivee; // Vient de config.h
      
-     // Permet de connaitre l'etat de la consigne actuelle ( on est arrivee ou non... )
-     bool arrivee;
+    // Permet de connaitre l'etat de la consigne actuelle ( on est arrivee ou non... )
+    bool arrivee;
 
-     
  };
  
  #endif
