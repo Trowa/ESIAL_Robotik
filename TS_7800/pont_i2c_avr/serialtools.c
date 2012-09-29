5f439ac13f88851493ea9ca0cbca6068e3e211a1
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2012-09-29 12:17:39 +0200

    Allez, soyons fous, mettons de la doc !

--- TS_7800/pont_i2c_avr/serialtools.c
+++ TS_7800/pont_i2c_avr/serialtools.c
@@ -4,6 +4,10 @@
 
 #define FOSC 8000000 // Oscillateur interne à 8.0 MHz
 
+/**
+ * Initialisation de la communication série
+ * @param baudrate Vitesse de la communication en Bauds
+ */
 void serial_init(uint32_t baudrate) {
   uint16_t ubbr = (FOSC/16/baudrate) - 1; //Calcul de la valeur du registre de sélection du baudrate
 
@@ -14,10 +18,20 @@ void serial_init(uint32_t baudrate) {
   UCSR0C = (3<<UCSZ00)|(0<<USBS0); // 8 bits par frame, 1 bit de stop
 }
 
+/**
+ * Envoie un caractère sur la série
+ * @param data Donnée à envoyer
+ */
 void serial_putc(uint8_t data) {
   while( !(UCSRnA & (1<<UDREn)) ); // Attente d'une éventuelle communication non terminée
   UDRn = data; // Met la donnée dans le buffer et commence l'envoi
 }
+
+/**
+ * Récupération d'un caractère sur la série
+ * La fonction est bloquante jusqu'à ce qu'il y ait un caractère à lire.
+ * @return Le caractère reçu
+ */
 uint8_t serial_getc(void) {
   while( !(UCSRnA & (1<<RXCn)) ); // On attend que la donnée arrive
   return UDRn // On renvoie ce qu'il y a dans le buffer
