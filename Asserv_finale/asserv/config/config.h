1ef9430893f337ee6d778e5a59ff597183b35d23
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2012-09-29 11:20:09 +0200

    Passage de toutes les varaibles de configuration en "extern" dans config.h
    Ce fichier est toujours celui à inclure dans les autres modules.
    Les variables sont à régler dans le nouveau fichier config.cpp
    TODO: tester le débug pour savoir si on peut à présent modifier
    la config à chaud.

--- Asserv_finale/asserv/config/config.h
+++ Asserv_finale/asserv/config/config.h
@@ -4,7 +4,7 @@
 #include "mbed.h"
 
   // Retour PC
-  static Serial pc(USBTX, USBRX);
+  extern Serial pc;
 
 
   //Débug ou pas ?
@@ -13,8 +13,8 @@
 /***************************
 *       CommandManager     *
 ****************************/
-  static double angleThreshold = 0.1; // Fenêtre de l'angle dans lequel on considère que le GoTo peut commencer à avancer
-  static int64_t returnThreshold = 50; // Distance en dessous de laquelle on revient en marche arriere si on dépasse, sinon demi-tour
+  extern double angleThreshold; // Fenêtre de l'angle dans lequel on considère que le GoTo peut commencer à avancer
+  extern int64_t returnThreshold; // Distance en dessous de laquelle on revient en marche arriere si on dépasse, sinon demi-tour
 
 /***************************
 *       ODOMETRIE          *
@@ -23,62 +23,62 @@
   #define PI 3.14159265358979
 
   // Définition des variables pour l'odométrie
-  static double frontParMetreCodeurG = 25650.333; // Nombre de tics codeurs en 1m pour codeur gauche
-  static double frontParMetreCodeurD = 25659.0; // Nombre de tics codeurs en 1m pour codeur droite
-  static int64_t distRoues = 285; // Distance entre les roues codeuses en mm
-  static int64_t uOParFront = 512; // Nombre d'UO pour un tic de codeur
+  extern double frontParMetreCodeurG; // Nombre de tics codeurs en 1m pour codeur gauche
+  extern double frontParMetreCodeurD; // Nombre de tics codeurs en 1m pour codeur droite
+  extern int64_t distRoues; // Distance entre les roues codeuses en mm
+  extern int64_t uOParFront; // Nombre d'UO pour un tic de codeur
 
   //Booléens gérant une eventuelle inversion des codeurs
-  static bool inverseCodeurG = true;
-  static bool inverseCodeurD = false;
-  static bool swapCodeurs = false;
+  extern bool inverseCodeurG;
+  extern bool inverseCodeurD;
+  extern bool swapCodeurs;
 
 /***************************
 *         Moteurs          *
 ****************************/
-  static int64_t V_MAX_POS_MOTOR = 90;   // MD22 : 1 a 127, vitesse maximum positive
-  static int64_t V_MAX_NEG_MOTOR = -90;  // MD22 : -1 a -128, vitesse maximum negative
+  extern int64_t V_MAX_POS_MOTOR;   // MD22 : 1 a 127, vitesse maximum positive
+  extern int64_t V_MAX_NEG_MOTOR;  // MD22 : -1 a -128, vitesse maximum negative
 
   //Booléens gerant une éventuelle inversion des moteurs
-  static bool inverseMoteurG = true;
-  static bool inverseMoteurD = true;
-  static bool swapMoteurs = false;
+  extern bool inverseMoteurG;
+  extern bool inverseMoteurD;
+  extern bool swapMoteurs;
 
 
 /***************************
 *           PID            *
 ****************************/
   // PID en distance
-  static int64_t DIST_KP = 70; // Coeff proportionelle
-  static int64_t DIST_KI = 0; // Coeff intégrale
-  static int64_t DIST_KD = 500; // Coeff dérivée
-  static double DIST_OUT_RATIO = 0.00001; // Coeff permettant de diminuer les valeurs du PID
-  static int64_t DIST_MAX_OUTPUT = 90; // Valeur de sortie maximum pour le moteur
-  static int64_t DIST_MAX_INTEGRAL = 0; // Valeur maximum de l'intégrale (0 = filtre PD)
+  extern int64_t DIST_KP; // Coeff proportionelle
+  extern int64_t DIST_KI; // Coeff intégrale
+  extern int64_t DIST_KD; // Coeff dérivée
+  extern double DIST_OUT_RATIO; // Coeff permettant de diminuer les valeurs du PID
+  extern int64_t DIST_MAX_OUTPUT; // Valeur de sortie maximum pour le moteur
+  extern int64_t DIST_MAX_INTEGRAL; // Valeur maximum de l'intégrale (0 = filtre PD)
 
 
-  static int64_t ANGLE_KP = 150; // Coeff proportionelle
-  static int64_t ANGLE_KI = 0; // Coeff intégrale
-  static int64_t ANGLE_KD = 950; // Coeff dérivée
-  static double ANGLE_OUT_RATIO = 0.000005; // Coeff permettant de diminuer les valeurs du PID
-  static int64_t ANGLE_MAX_OUTPUT = 90; // Valeur de sortie maximum pour le moteur
-  static int64_t ANGLE_MAX_INTEGRAL = 0; // Valeur maximum de l'intégrale (0 = filtre PD)
+  extern int64_t ANGLE_KP; // Coeff proportionelle
+  extern int64_t ANGLE_KI; // Coeff intégrale
+  extern int64_t ANGLE_KD; // Coeff dérivée
+  extern double ANGLE_OUT_RATIO; // Coeff permettant de diminuer les valeurs du PID
+  extern int64_t ANGLE_MAX_OUTPUT; // Valeur de sortie maximum pour le moteur
+  extern int64_t ANGLE_MAX_INTEGRAL; // Valeur maximum de l'intégrale (0 = filtre PD)
 
   // QUADRAMPDerivee
-  static int64_t DIST_QUAD_1ST_POS = 800000; // Vitesse max en marche avant
-  static int64_t DIST_QUAD_1ST_NEG = 800000; // Vitesse max en marche arrière
-  static int64_t DIST_QUAD_AV_2ND_ACC = 6000; // Accélération max en marche avant
-  static int64_t DIST_QUAD_AV_2ND_DEC = 4000; // Décélération max en marche avant
-  static int64_t DIST_QUAD_AV_ANTICIPATION_GAIN_COEF = 115; // Coeff déterminant le début de la rampe de décélération en marche avant
-  static int64_t DIST_QUAD_AR_2ND_ACC = 3000; // Accélération max en marche arrière
-  static int64_t DIST_QUAD_AR_2ND_DEC = 3000; // Décélération max en marche arrière
-  static int64_t DIST_QUAD_AR_ANTICIPATION_GAIN_COEF = 95; // Coeff déterminant le début de la rampe de décélération en marche arrière
-  static int64_t DIST_TAILLE_FENETRE_ARRIVEE = 200000; // Largeur de la zone où l'on considère être arrivé (UO)
+  extern int64_t DIST_QUAD_1ST_POS; // Vitesse max en marche avant
+  extern int64_t DIST_QUAD_1ST_NEG; // Vitesse max en marche arrière
+  extern int64_t DIST_QUAD_AV_2ND_ACC; // Accélération max en marche avant
+  extern int64_t DIST_QUAD_AV_2ND_DEC; // Décélération max en marche avant
+  extern int64_t DIST_QUAD_AV_ANTICIPATION_GAIN_COEF; // Coeff déterminant le début de la rampe de décélération en marche avant
+  extern int64_t DIST_QUAD_AR_2ND_ACC; // Accélération max en marche arrière
+  extern int64_t DIST_QUAD_AR_2ND_DEC; // Décélération max en marche arrière
+  extern int64_t DIST_QUAD_AR_ANTICIPATION_GAIN_COEF; // Coeff déterminant le début de la rampe de décélération en marche arrière
+  extern int64_t DIST_TAILLE_FENETRE_ARRIVEE; // Largeur de la zone où l'on considère être arrivé (UO)
 
-  static int64_t ANGLE_QUAD_1ST_POS = 700000; // Vitesse max en rotation
-  static int64_t ANGLE_QUAD_2ND_ACC = 6000; // Accélération max en rotation
-  static int64_t ANGLE_QUAD_2ND_DEC = 6000; // Décélération max en rotation
-  static int64_t ANGLE_QUAD_ANTICIPATION_GAIN_COEF = 35; // Coeff déterminant le début de la rampe de décélération en rotation
-  static int64_t ANGLE_TAILLE_FENETRE_ARRIVEE = 100000; // Largeur de la zone où l'on considère être arrivé (UO)
+  extern int64_t ANGLE_QUAD_1ST_POS; // Vitesse max en rotation
+  extern int64_t ANGLE_QUAD_2ND_ACC; // Accélération max en rotation
+  extern int64_t ANGLE_QUAD_2ND_DEC; // Décélération max en rotation
+  extern int64_t ANGLE_QUAD_ANTICIPATION_GAIN_COEF; // Coeff déterminant le début de la rampe de décélération en rotation
+  extern int64_t ANGLE_TAILLE_FENETRE_ARRIVEE; // Largeur de la zone où l'on considère être arrivé (UO)
 
 #endif
