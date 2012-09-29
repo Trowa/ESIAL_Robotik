54fd41d454544bf326a2e7db6a1f286168a16c5c
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2012-09-29 13:54:24 +0200

    D'après la doc de l'AVR, l'I2C n'est pas aussi simple que ça
    Il faut séparer les envois d'adresses et de données
    Ajout d'ne fonction pour initialiser la vitesse de transfert

--- TS_7800/pont_i2c_avr/i2ctools.c
+++ TS_7800/pont_i2c_avr/i2ctools.c
@@ -2,9 +2,18 @@
 #include <avr/io.h>
 
 
-#define START       0x08  //TWI start
-#define REP_START    0x10   //TWI repeat start
-#define MT_SLA_ACK    0x18   //slave ACK has been received
+#define START 0x08 // I2C start
+#define REP_START 0x10 // I2C repeat start
+#define SLA_W_ACK 0x18 // ACK adresse (SLA+W)
+#define SLA_DATA_ACK 0x28 // ACK données
+#define I2C_FREQ_DIV 8 //Diviseur de fréquence pour l'I2C (8 -> 250000 bauds)
+
+/**
+ * Initialise le bus I2C
+ */
+void i2c_init() {
+  TWBR = I2C_FREQ_DIV;
+}
 
 /**
  * Envoie la condition "start" sur l'I2C
@@ -26,14 +35,26 @@ int i2c_stop() {
 }
 
 /**
+ * Envoie l'adresse du périphérique ciblé sur l'I2C
+ * @param data Donnée à envoyer
+ * @return 0 si tout s'est bien passé
+ */
+int i2c_write_addr(uint8_t address) {
+  TWDR = address; //On charge le registre
+  TWCR = (1<<TWINT) | (1<<TWEN); //Transmission
+  while (!(TWCR & (1<<TWINT))); //Attente transmission...
+  return ((TWSR & 0xF8) != SLA_W_ACK); //Vérification d'erreur
+}
+
+/**
  * Envoie un octet sur l'I2C
  * @param data Donnée à envoyer
  * @return 0 si tout s'est bien passé
  */
-int i2c_write(uint8_t data) {
+int i2c_write_data(uint8_t data) {
   TWDR = data; //On charge le registre
   TWCR = (1<<TWINT) | (1<<TWEN); //Transmission
   while (!(TWCR & (1<<TWINT))); //Attente transmission...
-  return ((TWSR & 0xF8) != MT_SLA_ACK); //Vérification d'erreur
+  return ((TWSR & 0xF8) != SLA_DATA_ACK); //Vérification d'erreur
 }
 
--- TS_7800/pont_i2c_avr/i2ctools.h
+++ TS_7800/pont_i2c_avr/i2ctools.h
@@ -3,8 +3,10 @@
 
 #include <avr/io.h>
 
+void i2c_init(void);
 int i2c_start(void);
-int i2c_write(uint8_t data);
+int i2c_write_addr(uint8_t address);
+int i2c_write_data(uint8_t data);
 int i2c_stop(void);
 
 #endif //I2CTOOLS_H
