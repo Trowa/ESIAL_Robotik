440b922186a95e8b134b3962d24df4967f084bbe
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2016-03-28 18:13:43 +0200

    [IA] Correction de la simplification de route
    La régression linéaire ne fonctionne pas très bien quand les valeurs
    de x ou de y sont fixes : c'est un cas particulier à traiter

--- ia-java/src/navigation/RouteSimplificator.java
+++ ia-java/src/navigation/RouteSimplificator.java
@@ -60,6 +60,14 @@ public class RouteSimplificator {
       simpleRoute.add(complicatedRoute.get(0));
       ligneDroiteCourante.add(complicatedRoute.get(0));
       
+      /* On enregistre les valeurs en x et y: si x ou y sont fixes, l'interpolation ne
+       * fonctionne pas mais les points sont bien alignés
+       */
+      int currentX = complicatedRoute.get(0).x;
+      int currentY = complicatedRoute.get(0).y;
+      boolean xFixe = true;
+      boolean yFixe = true;
+      
       // On initialise l'interpolateur avec la droite courante (un seul point pour le moment)
       regression.setData(ligneDroiteCourante);
       
@@ -74,46 +82,71 @@ public class RouteSimplificator {
         ligneDroiteCourante.add(currentPoint);
         regression.addPoint(currentPoint);
         
-        // On teste la qualité de l'interpolation
-        if(regression.getCorrelCoeffAbs() >= dernierCoeffInterp) {
-          // Le point ajouté a amélioré l'interpolation : il est bien aligné sur la droite
-          dernierPointCorrectIndex = i;
-          dernierCoeffInterp = regression.getCorrelCoeffAbs();
-          
+        if(xFixe && currentX != currentPoint.x) {
+        	// x n'est plus fixe
+        	xFixe = false;
+        }
+        
+        if(yFixe && currentY != currentPoint.y) {
+        	// y n'est plus fixe
+        	yFixe = false;
+        }
+        
+        if(xFixe || yFixe) {
+        	// x  ou y est fixe, on va ignorer la sortie de la régression linéaire
+        	// On considère que le point améliore l'interpolation, vu que tout est aligné
+        	dernierPointCorrectIndex = i;
+        	// La qualité maximale est de 1
+        	dernierCoeffInterp = 1.0;
         } else {
-          /* 
-           * Le dernier point dévie de l'interpolation ; calcul de la distance
-           * entre le point courant et la droite d'interpolation
-           */
-          double distance = Math.abs((regression.getBeta1() * currentPoint.getX())
-                                  - currentPoint.getY() + regression.getBeta0())
-                            / Math.sqrt(1 + (regression.getBeta1() * regression.getBeta1()));
-          
-          
-          if(distance > errorMax) {
-            /* 
-             * Le point est trop loin de l'interpolation. On arrête la ligne au dernier
-             * point qui avait amélioré l'interpolation
-             */
-            
-            // On reprend le calcul à partir de ce point
-            i = dernierPointCorrectIndex;
-            
-            // On l'ajoute à la route simple
-            simpleRoute.add(complicatedRoute.get(dernierPointCorrectIndex));
-            
-            // On réinitialise la ligne courante
-            ligneDroiteCourante.clear();
-            ligneDroiteCourante.add(complicatedRoute.get(dernierPointCorrectIndex));
-            
-            // On reprend le calcul d'interpolation
-            regression.setData(ligneDroiteCourante);
-            dernierCoeffInterp = 0.0;
-            
-          } else {
-            // Le dernier point n'est pas loin de l'interpolation, on continue
-            dernierCoeffInterp = regression.getCorrelCoeffAbs();
-          }
+        
+	        // On teste la qualité de l'interpolation
+	        if(regression.getCorrelCoeffAbs() >= dernierCoeffInterp && dernierCoeffInterp > 0.5) {
+	          // Le point ajouté a amélioré l'interpolation : il est bien aligné sur la droite
+	          dernierPointCorrectIndex = i;
+	          dernierCoeffInterp = regression.getCorrelCoeffAbs();
+	          
+	        } else {
+	          /* 
+	           * Le dernier point dévie de l'interpolation ; calcul de la distance
+	           * entre le point courant et la droite d'interpolation
+	           */
+	          double distance = Math.abs((regression.getBeta1() * currentPoint.getX())
+	                                  - currentPoint.getY() + regression.getBeta0())
+	                            / Math.sqrt(1 + (regression.getBeta1() * regression.getBeta1()));
+	          
+	          
+	          if(distance > errorMax) {
+	            /* 
+	             * Le point est trop loin de l'interpolation. On arrête la ligne au dernier
+	             * point qui avait amélioré l'interpolation
+	             */
+	            
+	            // On reprend le calcul à partir de ce point
+	            i = dernierPointCorrectIndex;
+	            
+	            // On l'ajoute à la route simple
+	            simpleRoute.add(complicatedRoute.get(dernierPointCorrectIndex));
+	            
+	            // On réinitialise la ligne courante
+	            ligneDroiteCourante.clear();
+	            ligneDroiteCourante.add(complicatedRoute.get(dernierPointCorrectIndex));
+	            
+	            // On reprend le calcul d'interpolation
+	            regression.setData(ligneDroiteCourante);
+	            dernierCoeffInterp = 0.0;
+	            
+	            // On reconsidère que x et y sont fixes pour le moment
+	            currentX = complicatedRoute.get(dernierPointCorrectIndex).x;
+	            currentY = complicatedRoute.get(dernierPointCorrectIndex).y;
+	            xFixe = true;
+	            yFixe = true;
+	            
+	          } else {
+	            // Le dernier point n'est pas loin de l'interpolation, on continue
+	            dernierCoeffInterp = regression.getCorrelCoeffAbs();
+	          }
+	        }
         }
         
         // On passe au point suivant
