1c1413058a507e71fe1cea78a718aa4e1c88d3e3
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2016-03-29 16:28:20 +0200

    [IA] Test d'A* un peu plus compliqué

--- ia-java/src/navigation/astar/AStarTest.java
+++ ia-java/src/navigation/astar/AStarTest.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 import navigation.Point;
-import navigation.RouteSimplificator;
 
 /**
  * Classe pour tester l'Astar
@@ -23,45 +22,41 @@ public class AStarTest {
 		Stack<Point> chemin = null;
 		
 		for(int x = dimX / 4; x < dimX; x++) {
-			astar.setDefinitivelyAccessible(x, dimY/2, false);			
+			astar.setDefinitivelyAccessible(x, dimY/2 - dimY/10, false);			
+			astar.setDefinitivelyAccessible(x, dimY/2 + dimY/10, false);
 		}
 		
-		astar.updateVoisinageInfo();
+		for(int x = 0; x < 3 * dimX / 4; x++) {
+			astar.setDefinitivelyAccessible(x, dimY/2, false);
+		}
 		
 		int nbInteration = 10;
-		long elapsedTime = 0;
-		
-		for(int i = 0; i < nbInteration; i++) {
-			long startTime = System.currentTimeMillis();
-			chemin = astar.getChemin(new Point(dimX/2 - dimX/3, dimY/2 - dimY/3), new Point(dimX/2 + dimX/3, dimY/2 + dimY/3));
-			long stopTime = System.currentTimeMillis();
-			elapsedTime += (stopTime - startTime);
-		}
-		elapsedTime /= nbInteration;
-		
-		System.out.println("Chemin calculé en " + elapsedTime + "ms (moyenne)");
-			
-		ArrayList<Point> cheminList = new ArrayList<Point>(chemin.size());
-		while(!chemin.isEmpty()) {
-			cheminList.add(chemin.pop());
-		}
-		
-		int i = 0;
-		for(Point p : cheminList) {
-			System.out.println(" " + (i++) + " : " + p);
-		}
-		
-		RouteSimplificator simplificator = new RouteSimplificator(2);
-		
-		ArrayList<Point> cheminListSimple = simplificator.getSimplifiedRoute(cheminList);
+		astar.updateVoisinageInfo();
 
-		i = 0;
-		for(Point p : cheminListSimple) {
-			System.out.println(" " + (i++) + " : " + p);
+		long elapsedTime = 0;
+		long startTime = System.currentTimeMillis();
+	
+		chemin = astar.getChemin(new Point(dimX/2 - dimX/3, dimY/2 - dimY/3), new Point(dimX/2 + dimX/3, dimY/2 + dimY/3));
+
+		long stopTime = System.currentTimeMillis();
+		elapsedTime = (stopTime - startTime);
+
+		System.out.println("Chemin calculé en " + elapsedTime + "ms");
+		
+		if(chemin != null) {
+			ArrayList<Point> cheminList = new ArrayList<Point>(chemin.size());
+			while(!chemin.isEmpty()) {
+				cheminList.add(chemin.pop());
+			}
+			
+			int i = 0;
+			for(Point p : cheminList) {
+				System.out.println(" " + (i++) + " : " + p);
+			}
+		} else {
+			System.out.println("Pas de chemin !");
 		}
-		
-		System.out.println("Terminé !");
-		
+
 	}
 
 }
