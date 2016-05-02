974ec392fa1ba3f5465a603eb1fac614ccaf4358
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2016-05-02 19:17:52 +0200

    [IA] Une asserv interruptible

--- ia-java/src/api/asserv/Asserv.java
+++ ia-java/src/api/asserv/Asserv.java
@@ -16,62 +16,22 @@ import navigation.Point;
  *
  */
 
-public abstract class Asserv {
+public abstract class Asserv implements Runnable {
 	
 	/**
 	 * Booléen signalant l'exécution complète de la dernière commande
 	 */
 	protected boolean lastCommandFinished;
-
+	
+	/**
+	 * Booléen signalant l'interruption de l'asserv
+	 */
+	private boolean interrupted;
+	
 	/**
 	 * Position courante du robot
 	 */
 	protected Point nous;
-	
-	/**
-	 * Thread pour parser la sortie de l'asserv
-	 */
-	Thread checker = new Thread(new Runnable() {
-		@Override
-		public void run() {
-			int lastD = 2;
-			
-			while (true) {
-				String check = null;
-				// Lit une ligne
-				try {
-					check = readAsservOutput();
-				} catch (Exception e) {
-					e.printStackTrace();
-					return;
-				}
-				
-				// La ligne contient-elle des trucs intéressants ?
-				if (check.isEmpty() || check.charAt(0) != '#') {
-					continue;
-				}
-				
-				// On parse la ligne
-				try {
-					Pattern p = Pattern.compile("#x([0-9.-]+)y([0-9.-]+)a([0-9.-]+)d([0-2])");
-					Matcher m = p.matcher(check);
-					if (m.find()) {
-						if (m.group(4).equals("1") || (lastD == 0 && m.group(4).equals("2"))) {
-							System.out.println("finished !!");
-							lastCommandFinished = true;
-						}
-						
-						Point point = new Point((int)Double.parseDouble(m.group(1)), (int)Double.parseDouble(m.group(2)));
-						point.setCap(Double.parseDouble(m.group(3)));
-						nous = point;
-						lastD = Integer.parseInt(m.group(4));
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	});
 	
 	/**
 	 * Initialise l'asserv
@@ -81,13 +41,14 @@ public abstract class Asserv {
 	 */
 	protected void asservInit() {
 		reset();
+		
+		// On démarre le thread pour parser la sortie de l'asser
+		Thread checker = new Thread(this);
 		checker.start();
 	}
 	
 	/**
-	 * Renvoit un booléen signalant si la dernière commande est totalement
-	 * exécuté
-	 * 
+	 * Renvoie un booléen signalant si la dernière commande est totalement exécuté
 	 * @return lastCommandFinished
 	 */
 	public boolean isLastCommandFinished() {
@@ -95,18 +56,58 @@ public abstract class Asserv {
 	}
 	
 	/**
+	 * Permet de savoir si l'asserv a été interrompue par un évènement extérieur 
+	 * @return true si l'asserv a été interrompu
+	 */
+	public boolean isInterrupted() {
+		return interrupted;
+	}
+	
+	/**
 	 * Attend que la dernière commande ait fini son exécution 
 	 */
-	public void waitForFinish() {
-		while (!lastCommandFinished) {
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+	public void waitForFinish() throws InterruptedException {
+		
+		synchronized(this) {
+			while (!lastCommandFinished && !interrupted) {
+				this.wait();
+				System.out.println("Réveil asserv!");
+			}
+			if(interrupted) {
+				throw new InterruptedException("Pas eu le temps de finir la commande!");
 			}
 		}
 	}
 	
+	/**
+	 * Attend que la dernière commande ait fini son exéction,
+	 * avec un timeout
+	 * @param timeout le timeout en millisecondes
+	 * @return true si la commande a été exécutée avant la fin du timeout
+	 * @throws InterruptedException si un événement externe a interrompu l'asserv
+	 */
+	public boolean waitForFinishTimeout(int timeout) throws InterruptedException {
+		
+		boolean completed = false;
+		int remainingTime = timeout;
+		
+		synchronized(this) {
+			while (!lastCommandFinished && !interrupted && remainingTime > 0) {
+				long startTime = System.currentTimeMillis();
+				this.wait(remainingTime);
+				long endTime = System.currentTimeMillis();
+				System.out.println("Réveil asserv!");
+				remainingTime = remainingTime - ((int)(endTime - startTime));
+			}
+			if(interrupted) {
+				throw new InterruptedException("Pas eu le temps de finir la commande!");
+			} else if(lastCommandFinished) {
+				completed = true;
+			}
+		}
+		
+		return completed;
+	}
 	
 	/**
 	 * Le robot s'aligne avec la position (x,y) puis y va
@@ -115,7 +116,7 @@ public abstract class Asserv {
 	 * @param y Ordonnée en mm
 	 * @param wait Attendre que la commande soit terminée avant de retourner
 	 */
-	public void gotoPosition(double x, double y, boolean wait) {
+	public void gotoPosition(double x, double y, boolean wait) throws InterruptedException {
 		String commande = "g"+x+"#"+y+"\n";
 		synchronized(this) {
 			sendCommand(commande);
@@ -133,7 +134,7 @@ public abstract class Asserv {
 	 * @param y Ordonnée en mm
 	 * @param wait Attendre que la commande soit terminée avant de retourner
 	 */
-	public void face(double x, double y, boolean wait) {
+	public void face(double x, double y, boolean wait) throws InterruptedException {
 		String commande = "f"+x+"#"+y+"\n";
 		synchronized(this) {
 			sendCommand(commande);
@@ -148,7 +149,7 @@ public abstract class Asserv {
 	 * @param d Distance à parcourir en mm
 	 * @param wait Attendre que la commande soit terminée avant de retourner
 	 */
-	public void go(double d, boolean wait) {
+	public void go(double d, boolean wait) throws InterruptedException {
 		String commande = "v"+d+"\n";
 		synchronized(this) {
 			sendCommand(commande);
@@ -164,7 +165,7 @@ public abstract class Asserv {
 	 * @param a Angle à parcourir en degrés 
 	 * @param wait Attendre que la commande soit terminée avant de retourner
 	 */
-	public synchronized void turn(double a, boolean wait) {
+	public void turn(double a, boolean wait) throws InterruptedException {
 		String commande = "t"+a+"\n";
 		synchronized(this) {
 			sendCommand(commande);
@@ -196,13 +197,28 @@ public abstract class Asserv {
 	 */
 	public void halt() {
 		System.out.println("JE ME HALT");
-		sendCommand("h");
+		
+		// On réserve le moniteur de l'asserv
+		synchronized(this) {
+			sendCommand("h");
+			
+			// Si on n'était pas interrompu
+			if(!interrupted) {
+				interrupted = true; // On s'arrête,
+				this.notifyAll(); // et on réveille les threads qui attendaient
+				
+				/* On rend la main de force, pour que les threads qui attendent
+				 * puissent avoir la main et traiter le problème */
+				Thread.yield();
+			}
+		}
 	}
 	
 	/**
 	 * Redémarre l'asservissement après un halt()
 	 */
 	public void resetHalt() {
+		interrupted = false;
 		sendCommand("r");
 	}
 
@@ -220,10 +236,11 @@ public abstract class Asserv {
 	public void reset() {
 		sendCommand("R"); // autrefois il y avait un break...
 		lastCommandFinished = true;
+		interrupted = false;
 		while(true) {
 			String blabla = readAsservOutput();
 			System.out.println("blabla: " + blabla);
-			if (blabla.endsWith("ok")) {
+			if (blabla.contains("ok")) {
 				System.out.println("Asserv ready (la salope)");
 				return;
 			}
@@ -250,5 +267,52 @@ public abstract class Asserv {
 	 * @return la ligne lue
 	 */
 	protected abstract String readAsservOutput();
+	
+	/**
+	 * Le code qui permet de parser la sortie de l'asserv
+	 */
+	public void run() {
+		int lastD = 2;
+		
+		while (true) {
+			String check = null;
+			// Lit une ligne
+			try {
+				check = readAsservOutput();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+			
+			// La ligne contient-elle des trucs intéressants ?
+			if (check.isEmpty() || check.charAt(0) != '#') {
+				continue;
+			}
+			
+			// On parse la ligne
+			try {
+				Pattern p = Pattern.compile("#x([0-9.-]+)y([0-9.-]+)a([0-9.-]+)d([0-2])");
+				Matcher m = p.matcher(check);
+				if (m.find()) {
+					if (m.group(4).equals("1") || (lastD == 0 && m.group(4).equals("2"))) {
+						System.out.println("finished !!");
+						
+						synchronized(this) {
+							lastCommandFinished = true;
+							this.notifyAll();
+						}
+						
+					}
+					
+					Point point = new Point((int)Double.parseDouble(m.group(1)), (int)Double.parseDouble(m.group(2)));
+					point.setCap(Double.parseDouble(m.group(3)));
+					nous = point;
+					lastD = Integer.parseInt(m.group(4));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 }
